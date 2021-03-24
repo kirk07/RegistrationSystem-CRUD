5a58cea964deb57c01228cf60a69ae0bbacaf4e2
kirk07 <78890731+kirk07@users.noreply.github.com>
2021-03-24 18:50:26 +0800

    Add files via upload

---
+++ RegistrationSystem/Pages/Index.cshtml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using RegistrationSystem.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+
+namespace RegistrationSystem.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public IndexModel(RegistrationDBContext PersonDBcontext)
+        {
+            _persondbcontext = PersonDBcontext;
+        }
+        //create property for DBContext instance
+        private readonly RegistrationDBContext _persondbcontext;
+
+        [BindProperty]
+        public Person Register { get; set; }
+
+        public List<Person> Persons = new List<Person>();
+        public void OnGet()
+        {
+            Persons = _persondbcontext.Persons.ToList();
+        }
+
+        public void OnGetDelete(int id)
+        {
+            //query the student to delete
+            var person = _persondbcontext.Persons
+                .FirstOrDefault(s => s.PersonID == id);
+
+            //perform null check
+            if (person != null)
+            {
+                //remove and update DB
+                _persondbcontext.Persons.Remove(person);
+                _persondbcontext.SaveChanges();
+            }
+
+
+            //refresh the page
+            OnGet();
+
+        }
+    }
+}
